89f5060041d9e720ae6aa42055084119bea4e3ab
Rose 3 <LeahYesX@gmail.com>
2024-01-26 13:14:31 -0500

    Add

---
+++ installer.py
@@ -0,0 +1,31 @@
+import os
+import json
+import requests
+import shutil
+
+mc_folder = os.path.expanduser("~\\AppData\\Roaming\\.minecraft")
+mod_file = "https://raw.githubusercontent.com/RoseyKat/mc-mods/main/mods/mods.zip"
+
+with open("data/config.json", "r") as f:
+    data = json.loads(f.read())
+
+    essentials = data["essentials"]
+    mod_type = data["mod_type"]
+    version = data["version"]
+    rm_old = data["rm_old"]
+
+if essentials:
+    path = f"{mc_folder}\\essential_mod\\fabric\\{version}\\{version} Fabric Essential\\mods"
+else:
+    path = f"{mc_folder}\\mods"
+
+if rm_old:
+    shutil.rmtree(path)
+    os.makedirs(path)
+
+downloaded_file = requests.get(mod_file)
+os.makedirs("cache", exist_ok=True)
+with open("cache/mods.zip", "wb") as f:
+    f.write(downloaded_file.content)
+
+shutil.unpack_archive("cache/mods.zip", path)
